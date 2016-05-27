2464d65a46c12c776910a2d90a75cd09842acff9
andressanchez1 <guitox@msn.com>
2016-05-26 19:28:54 -0500

    Add files via upload

---
+++ ejercicioultimatarea.R
@@ -0,0 +1,12 @@
+data(BCI)
+mod <- decorana(BCI)
+plot(mod)
+names(BCI)[1:5]
+shnam <- make.cepnames(names(BCI))
+shnam[1:5]
+pl <- plot(mod, dis="sp")
+identify(pl, "sp", labels=shnam)
+stems <- colSums(BCI)
+plot(mod, dis="sp", type="n")
+sel<- orditorp(mod, dis="sp", lab=shnam, priority=stems, pcol = "grey")
+                
